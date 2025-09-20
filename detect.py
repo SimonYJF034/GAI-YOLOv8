7461efbecd8b7c962fca4eb2ba1203764515ef30
ASD123 <yjf1156618837@gmail.com>
2025-09-21 07:40:56 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -3,8 +3,11 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 
     # Load a model
-    model = YOLO(model=r'C:\Users\11566\Desktop\ultralytics-main\runs\train\exp32\weights\best.pt')
-    model.predict(source=r'C:/Users/11566/Desktop/seg-data/images/test',
+    model = YOLO(model=r'GAI-YOLOv8.pt') #model path or triton URL
+    model.predict(source=r'dataset/images/test', #images path
+                  imasz=640,
+                  project='runs/detect',
+                  name='exp'
                   save=True,
                   show=False,
                   )
