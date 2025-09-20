31e027d9fdfd355f4c6bb6b4a531df3a69558210
ASD123 <yjf1156618837@gmail.com>
2025-09-21 07:36:43 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-@Auth ： 挂科边缘
-@File ：detect.py
-@IDE ：PyCharm
-@Motto:学习新思想，争做新青年
-@Email ：179958974@qq.com
-"""
-
 from ultralytics import YOLO
 
 if __name__ == '__main__':
